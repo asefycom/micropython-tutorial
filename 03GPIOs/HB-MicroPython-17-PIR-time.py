57c59b7978306730f818fa10e66cc10f16d98c14
mostafa-asefy <asefi.v@gmail.com>
2020-05-13 09:38:06 +0330

    Lesson 17, Time method

--- 03GPIOs/HB-MicroPython-17-PIR-time.py
+++ 03GPIOs/HB-MicroPython-17-PIR-time.py
@@ -1,26 +1,31 @@
 #Source.Code.From.HamRuyesh.com
 
 from machine import Pin
-from time import sleep
+from time import sleep, time
 
 motion = False
+start_timer = False
+last_motion_time = 0
+delay_interval = 10
 
 pir = Pin(14, Pin.IN)
 led = Pin(12, Pin.OUT)
 
 def interrupt_handler(pin):
-    global motion
+    global motion, interrupt_pin, last_motion_time, start_timer
     motion = True
-    global interrupt_pin
     interrupt_pin = pin
+    last_motion_time = time()
+    start_timer = True
 
 pir.irq(handler=interrupt_handler, trigger=Pin.IRQ_RISING)
 
 while True:
-    if motion:
+    if motion and start_timer:
         led.value(1)
         print("Led turned on! Interrupt sent by: ", interrupt_pin)
-        sleep(10)
+        start_timer = False
+    elif motion and (time() - last_motion_time) > delay_interval:  
         led.value(0)
         print("Motion Stopped!")
         motion = False
